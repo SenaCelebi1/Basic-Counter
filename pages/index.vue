787c074e4cdc075c0992c87296d0f9a384f8f61f
Sena Celebi <43573954+SenaCelebi@users.noreply.github.com>
2021-11-02 22:53:40 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="counter">
+  <h3>Counter: {{ counter }}</h3>
+  <button type="button" v-on:click="increase">Increase</button> 
+  <p></p>
+  <button type="button" v-on:click="decrease">Decrease</button> 
+
+</div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+               counter: 0            
+      }       
+    
+  },
+  methods: {
+    increase: function() {
+      this.counter++;
+    },
+    decrease: function() {
+      this.counter--;
+    }
+  }
+}
+</script>
